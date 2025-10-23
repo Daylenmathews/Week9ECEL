1d22a4f3be400e63cedc421e2502ff39fb949237
Daylenmathews <151976934+Daylenmathews@users.noreply.github.com>
2025-10-23 16:13:35 -0600

    Update multiplexer.v

--- multiplexer.v
+++ multiplexer.v
@@ -1,11 +1,11 @@
+
 module multiplexer(
 input [1:0] Sel,
 input [3:0]A, B, C, D, 
 input enable,
-output Y
+output [3:0]Y
     );
     
-    assign Y = (A & ~Sel[0] & ~Sel[1]| B & Sel[0] & ~Sel[1]|C & ~Sel[0] & Sel[1]|D & Sel[0] & Sel[1] ) & enable;
-
-
+    
+assign Y = enable ? (( Sel==0 ? A:(Sel==1 ? B:(Sel ==2 ? C:(Sel== 3 ? D: 0))))) : 0;
 endmodule
