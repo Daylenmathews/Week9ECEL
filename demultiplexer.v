b9686d6cac8bea9be02dca03862918b53e175254
Daylenmathews <151976934+Daylenmathews@users.noreply.github.com>
2025-10-23 15:36:30 -0600

    Update demultiplexer.v

--- demultiplexer.v
+++ demultiplexer.v
@@ -1,1 +1,15 @@
+module demultiplexer(
+input [1:0]Sel,
+input [3:0]in,
+input enable,
+output [3:0]A, B, C, D
+
+
+    );
+    assign A = enable ? ((Sel[0]& Sel[1]) ? in : 0) : 0;
+    assign B = enable ? ((~Sel[0]& Sel[1]) ? in : 0) : 0;
+    assign C = enable ? ((Sel[0]& ~Sel[1]) ? in : 0) : 0;
+    assign D = enable ? ((~Sel[0]& ~Sel[1]) ? in : 0) : 0;
+    
+endmodule
 
